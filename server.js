1d6c9fca52699a11b29b8e221b1379aa54559e36
aiai-rs <js5553@outlook.com>
2026-03-26 21:50:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const http = require('http'); // 新增
 const { Server } = require("socket.io"); // 新增
